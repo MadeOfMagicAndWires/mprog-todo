9d8990a0195a9ea2e844134063b3c00fb01f3286
Joost Bremmer <toost.b@gmail.com>
2015-11-17 16:42:18 +0100

    TODO: Start on ArrayAdapter

--- app/src/main/java/nl/mprog/todoloo/MainActivity.java
+++ app/src/main/java/nl/mprog/todoloo/MainActivity.java
@@ -5,9 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,4 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    ArrayAdapter<String> tasks_string;
+
+    /*
+    try
+    {
+        InputStream arraystream = getResources().openRawResource(R.raw.tasks);
+
+        InputStreamReader arrayreader = new InputStreamReader(arraystream);
+        BufferedReader arraybuffer = new BufferedReader(arrayreader);
+        String line;
+        while ((line = arraybuffer.readLine()) != null) {
+            tasks_string.add(line);
+            Log.v("line added", line);
+        }
+    }catch (IOException e1) {
+                e1.printStackTrace();
+    }
+
+    */
+    ArrayAdapter<String> tasks = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+
 }
