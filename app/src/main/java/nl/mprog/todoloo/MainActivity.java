e41924f1cd7df2dc9e03e5240d36097e0556e1e6
Joost Bremmer <toost.b@gmail.com>
2015-11-19 15:15:10 +0100

    Documentation and requirements
    + Add the required second fragment
    + More documentation comments for blocks of text

--- app/src/main/java/nl/mprog/todoloo/MainActivity.java
+++ app/src/main/java/nl/mprog/todoloo/MainActivity.java
@@ -1,11 +1,11 @@
 package nl.mprog.todoloo;
 
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -65,21 +65,27 @@ public class MainActivity extends AppCompatActivity {
      * Will also remove the dummy task 'Add a new task' if it was present.
      * @param view floating action button, not actually used.
      */
+
     public void addTask(View view) {
         EditText taskView = (EditText) findViewById(R.id.newtask_text);
         String newtask = taskView.getText().toString();
 
-        if(newtask.isEmpty()) {
-            Snackbar.make(taskView,R.string.empty_task, Snackbar.LENGTH_SHORT).show();
+        //if the new task is an empty string, show a message to the user and don't add anything
+        //to the task list. Otherwise, just add it to the task list
+        if(newtask.equals("")) {
+            Snackbar.make(taskView, R.string.empty_task, Snackbar.LENGTH_SHORT).show();
         }
         else {
             tasks.add(newtask);
 
+            //if there were no tasks present, remove the dummy task and
+            //update noTask boolean.
             if (noTasks) {
                 tasks.remove(tasks.remove(0));
                 noTasks = false;
             }
 
+            //Update listView and reset EditText
             tasksAdapter.notifyDataSetChanged();
             taskView.setText("");
         }
@@ -89,14 +95,14 @@ public class MainActivity extends AppCompatActivity {
      * Calls writeTasks() whenever the Activity reaches the onStop phase.
      */
     @Override
-    public void onStop() {
+    public void onStop(){
         super.onStop();
         if(!noTasks) {writeTasks();}
 
     }
 
     /**
-     *  Reads tasks from the file 'tasks' and initializes the List from it.
+     *  Tries to Read tasks from the file 'tasks' and initializes the List from it.
      *  Each line in the file corresponds to one task in the list.
      *  If the file is empty readTasks() provides a dummy task called "Add a new task"
      */
@@ -104,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
     public void readTasks() {
         Scanner scan;
 
+        //For each line in the file 'tasks' add the line to tasks ArrayList
         try {
             scan = new Scanner(openFileInput("tasks"));
             while (scan.hasNextLine()) {
@@ -114,25 +121,27 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        //If tasks list is empty, add the dummy task until something is added
         if(tasks.isEmpty()) {
             tasks.add(0, getString(R.string.no_tasks));
             noTasks = true;
         }
 
+        //Set the ListView adapter, which will make a list item for each task
         tasksAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
-
         ListView tasksList = (ListView) findViewById(R.id.list);
         tasksList.setAdapter(tasksAdapter);
+
+        //Set the ListView OnItemLongClickListener, will call removeTask on every long click
         tasksList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 TextView taskView = (TextView) view;
                 String task = String.valueOf(taskView.getText());
-                if(removeTask(task)) {
+                if (removeTask(task)) {
                     tasksAdapter.notifyDataSetChanged();
                     return true;
-                }
-                else {
+                } else {
                     return false;
                 }
             }
@@ -149,10 +158,12 @@ public class MainActivity extends AppCompatActivity {
      * @return true if the task was in the list and is now removed, false if otherwise.
      */
     public boolean removeTask(String task) {
+        //if task is in tasklist, remove it.
         if(tasks.contains(task)) {
             tasks.remove(tasks.indexOf(task));
             //Log.v("tasks", "removed task: " + task)
 
+            //If no task remains now, re-add the dummy task.
             if(tasks.isEmpty()) {
                 tasks.add(0, getString(R.string.no_tasks));
                 noTasks = true;
@@ -166,25 +177,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Writes the task list to the file 'tasks', one task per line.
+     * Tries to write the task list to the file 'tasks', one task per line.
      *
      */
     public void writeTasks() {
 
         PrintStream out;
         try{
+            //open file
             out = new PrintStream(openFileOutput("tasks", MODE_PRIVATE));
 
+            //Write each task to a new line in the file
             for(int i=0;i<tasks.size();i++) {
                 out.println(tasks.get(i));
                 //Log.v("tasks", "Wrote " + tasks.get(i));
             }
+            //finally close file
+            out.close();
+            Log.v("tasks", "saved tasks");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        Log.v("tasks", "saved tasks");
+
     }
 
 }
